e32cb378a308730ead99ca07cc0c0423fe79ed3d
MaxiVazquezg <maxivaz55@gmail.com>
2025-06-12 22:13:05 -0300

    a

--- src/config/bd.js
+++ src/config/bd.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
+  user: process.env.DB_USER || 'maxivaz',
   password: process.env.DB_PASS || 'Tiburcio123',
   database: process.env.DB_NAME || 'productos_db',
   waitForConnections: true,
